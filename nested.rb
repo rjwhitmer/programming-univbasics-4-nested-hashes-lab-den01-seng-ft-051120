760272087f0d879eebc63d76328c07fffeae72a2
dapper-module-0474 <rjwhitmercc@gmail.com>
2020-04-26 01:46:20 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -153,5 +153,10 @@ def adding_to_dennis
         :languages => ["C"]
       }
     }
+    programmer_hash[:dennis_ritchie][:languages] << "Assembly"
+    
+  new_dennis = programmer_hash
+  new_dennis
+  
 
 end
