74151ebba3e64368a177e6429e821570a57d7e95
dapper-module-0474 <rjwhitmercc@gmail.com>
2020-04-26 01:19:15 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -89,7 +89,6 @@ def adding_matz
         :languages => ["C"]
       }
     }
-    programmer_hash = {}
     programmer_hash[:yukihiro_matsumoto]
     programmer_hash[:yukihiro_matsumoto] << [:known_for => "Ruby"]
     programmer_hash[:yukihiro_matsumoto] << [:languages => "LISP"]
